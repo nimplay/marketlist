ab6e267c5f5eaef3971b7c2e946839644f025cc0
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-03-29 13:42:41 -0400

    save

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from views.main_view import MainView
-
+# from kivy.uix.screenmanager import ScreenManager, Screen
 class ShoppingApp(App):
     def build(self):
         return MainView()
