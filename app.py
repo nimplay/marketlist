1f6efd5a67c7f74416c223c8445f0d43be4d5f8d
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-03-30 18:56:26 -0400

    mantain racha

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from views.main_view import MainView
 # from kivy.uix.screenmanager import ScreenManager, Screen
+# commented out to avoid circular import issues
 class ShoppingApp(App):
     def build(self):
         return MainView()
